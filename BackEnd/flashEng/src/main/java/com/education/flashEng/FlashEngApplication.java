9eb704182f012cfd8d70b199fd0ee536b0628b6d
atphutals01 <nguyenkhactruong04@gmail.com>
2024-10-15 10:16:12 +0700

    add structure to BackEnd

---
+++ BackEnd/flashEng/src/main/java/com/education/flashEng/FlashEngApplication.java
@@ -0,0 +1,13 @@
+package com.education.flashEng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashEngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashEngApplication.class, args);
+	}
+
+}
